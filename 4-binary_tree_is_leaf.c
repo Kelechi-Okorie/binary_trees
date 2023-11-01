c5c1b2eb674f015a3a7b516b8fdaab0382f4315d
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-11-01 14:12:51 +0100

    Added task 4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: the node to check if it's a leaf
+ * Description: checks if a node is a leaf
+ *
+ * Return: 1 if @node is a leaf, 0 otherwise
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (1);
+
+	if (node->left || node->right)
+		return (0);
+
+	return (1);
+}
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ File 0-binary_tree_node.c - creates a binary tree node
 File 1-binary_tree_insert_left.c - inserts a node as the left-child of another node
 File 2-binary_tree_insert_right.c - inserts a node as the right-child of another node
 File 3-binary_tree_delete.c - deletes an entire binary tree
+File 4-binary_tree_is_leaf.c - checks if a node is a leaf
